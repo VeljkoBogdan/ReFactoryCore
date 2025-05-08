3eaedb7412db43cae48563bcdf213234ae88193e
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-05-08 12:39:52 +0200

    Added a Steam Multiblock Extractor "Large Steam Extractor"

--- src/main/java/com/illuminatijoe/refactorycore/data/datagen/ReFactoryLangGen.java
+++ src/main/java/com/illuminatijoe/refactorycore/data/datagen/ReFactoryLangGen.java
@@ -9,10 +9,13 @@ public class ReFactoryLangGen {
     public static void init(RegistrateLangProvider provider) {
         replace(provider, "block.gtceu.steam_blender", "Steam Blender");
         replace(provider, "block.gtceu.steam_borer", "Steam Borer");
+        replace(provider, "block.gtceu.large_steam_extractor", "Large Steam Extractor");
 
         replace(provider, "tooltip.gtceu.steam_blender.0", "Steam multiblock mixer");
         replace(provider, "tooltip.gtceu.steam_blender.1", "§7Processes up to 8 recipes in parallel");
         replace(provider, "tooltip.gtceu.steam_borer.0", "Steam multiblock lathe");
         replace(provider, "tooltip.gtceu.steam_borer.1", "§7Processes up to 8 recipes in parallel");
+        replace(provider, "tooltip.gtceu.steam_large_extractor.0", "Steam multiblock extractor");
+        replace(provider, "tooltip.gtceu.steam_large_extractor.1", "§7Processes up to 8 recipes in parallel");
     }
 }
--- src/main/java/com/illuminatijoe/refactorycore/machines/ReFactoryMachines.java
+++ src/main/java/com/illuminatijoe/refactorycore/machines/ReFactoryMachines.java
@@ -37,6 +37,7 @@ public class ReFactoryMachines {
                     .where('P', Predicates.blocks(CASING_BRONZE_PIPE.get()))
                     .where('C', Predicates.blocks(CASING_BRONZE_BRICKS.get())
                             .or(Predicates.abilities(PartAbility.STEAM).setPreviewCount(1))
+                            .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setPreviewCount(1))
                             .or(Predicates.abilities(PartAbility.STEAM_IMPORT_ITEMS).setPreviewCount(1))
                             .or(Predicates.abilities(PartAbility.STEAM_EXPORT_ITEMS).setPreviewCount(1))
                             .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setPreviewCount(1)))
@@ -68,6 +69,32 @@ public class ReFactoryMachines {
                     .build())
             .workableCasingRenderer(GTCEu.id("block/casings/solid/machine_casing_bronze_plated_bricks"),
                     GTCEu.id("block/machines/lathe"), false)
+            .register();
+
+    public static final MultiblockMachineDefinition MB_STEAM_EXTRACTOR = GTRegistration.REGISTRATE
+            .multiblock("steam_large_extractor", WeakSteamParallelMultiblockMachine::new)
+            .tooltips(Component.translatable("tooltip.gtceu.steam_large_extractor.0"),
+                    Component.translatable("tooltip.gtceu.steam_large_extractor.1"))
+            .rotationState(RotationState.ALL)
+            .appearanceBlock(BRONZE_HULL)
+            .recipeType(GTRecipeTypes.EXTRACTOR_RECIPES)
+            .recipeModifier(WeakSteamParallelMultiblockMachine::recipeModifier, true)
+            .addOutputLimit(ItemRecipeCapability.CAP, 1)
+            .pattern(definition -> FactoryBlockPattern.start()
+                    .aisle("CCC", "CCC", "CCC")
+                    .aisle("CCC", "CPC", "CPC")
+                    .aisle("COC", "CCC", "CCC")
+                    .where('O', Predicates.controller(Predicates.blocks(definition.getBlock())))
+                    .where('P', Predicates.blocks(CASING_BRONZE_PIPE.get()))
+                    .where('C', Predicates.blocks(CASING_BRONZE_BRICKS.get())
+                            .or(Predicates.abilities(PartAbility.STEAM).setPreviewCount(1))
+                            .or(Predicates.abilities(PartAbility.STEAM_IMPORT_ITEMS).setPreviewCount(1))
+                            .or(Predicates.abilities(PartAbility.STEAM_EXPORT_ITEMS).setPreviewCount(1))
+                            .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setPreviewCount(1))
+                            .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setPreviewCount(1)))
+                    .build())
+            .workableCasingRenderer(GTCEu.id("block/casings/solid/machine_casing_bronze_plated_bricks"),
+                    GTCEu.id("block/machines/extractor"), false)
             .register();
 
     public static void init() {}
